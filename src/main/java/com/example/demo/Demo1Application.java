f622860c069099eb8b294d04a0df0434c501dc75
1Camile <kamilakarwat22@gmail.com>
2025-10-21 14:30:47 +0200

    pierwszy

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo1Application {
-
+ddd
      void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
     }
